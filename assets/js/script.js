24de37fa614079c051461b5767d9c4ab8e2d162a
JothaCortes <j.cortesolivarez@gmail.com>
2024-08-20 13:26:16 -0400

    5 commit listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
-/*JQuery */
 
+
+const proyectos =[
+    {
+        name: 'Preuniversitario Cebal',
+        img:'./assets/img/Cebal2.jpeg',
+        texto: 'Sistema para la gestión de matriculas de un preuniversitario',
+    },
+    {
+        name: 'Colegio San Francisco',
+        img:'./assets/img/proyectosf.jpg',
+        texto: 'Sistema creado en GeneXus para la gestión de cursos de un colegio',
+    },
+    {
+        name: 'Sistema de tareas',
+        img:'./assets/img/proyectost.png',
+        texto: 'Sistema para la gestión de tareas',  
+    }
+];
+
+	
+function modalProyectos(i){
+    const modal = document.querySelector('#exampleModal');
+    console.log(modal);
+    const h1 = modal.querySelector('h1');
+    h1.innerHTML = proyectos[i].name;
+
+    const img = modal.querySelector('img');
+    img.setAttribute('src', proyectos[i].img);
+ 
+    const p = modal.querySelector('p');
+    p.innerHTML = proyectos[i].texto;
+}
 
 function llamarModal(){
     var nombre = document.getElementById("nombreInput").value;
